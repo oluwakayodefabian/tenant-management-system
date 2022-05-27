2500084e79c7f4da21cdfd6ed4628022ea6213bc
Owotoke Oluwakayode <oluwakayodefabian@gmail.com>
2022-05-27 17:17:13 +0100

    made changes

--- app/Views/Admin/dashboard.php
+++ app/Views/Admin/dashboard.php
@@ -31,7 +31,7 @@
                 <!-- Content Row -->
                 <div class="row">
 
-                    <!-- Earnings (Monthly) Card Example -->
+                    <!-- Admin users Card -->
                     <div class="col-xl-3 col-md-6 mb-4">
                         <div class="card border-left-primary shadow h-100 py-2">
                             <div class="card-body">
@@ -51,130 +51,62 @@
                         </div>
                     </div>
 
-                    <!-- Earnings (Monthly) Card Example -->
+                    <!-- Properties Card -->
                     <div class="col-xl-3 col-md-6 mb-4">
                         <div class="card border-left-success shadow h-100 py-2">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
                                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                            Categories</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800" id="numOfCategories">
-                                            <?= 4 ?>
+                                            Properties</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800" id="numOfProperties">
+                                            <?= 0 ?>
                                         </div>
                                     </div>
                                     <div class="col-auto">
-                                        <i class="fas fa-book-open fa-2x text-gray-300"></i>
+                                        <i class="fas fa-house-damage fa-2x text-gray-300"></i>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <!-- Earnings (Monthly) Card Example -->
+                    <!-- Tenants Card -->
                     <div class="col-xl-3 col-md-6 mb-4">
                         <div class="card border-left-info shadow h-100 py-2">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Posts
+                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tenants
                                         </div>
                                         <div class="row no-gutters align-items-center">
                                             <div class="col-auto">
-                                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800" id="numOfPosts">
-                                                    <?= 4 ?>
+                                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800" id="numOfTenants">
+                                                    <?= 0 ?>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-auto">
-                                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        <i class="fas fa-users fa-2x text-gray-300"></i>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <!-- Pending Requests Card Example -->
-                    <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-warning shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                            Sermons</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 4 ?></div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-book-open fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Number of departments card -->
+                    <!-- Number of Complaints card -->
                     <div class="col-xl-3 col-md-6 mb-4">
                         <div class="card border-left-danger shadow h-100 py-2">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
                                         <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">
-                                            Departments</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 5 ?></div>
+                                            Complaints</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 0 ?></div>
                                     </div>
                                     <div class="col-auto">
-                                        <i class="fas fa-users fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Number of donations Card Example -->
-                    <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-success shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                            Donations</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 7 ?></div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-donate fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Number of Events Card Example -->
-                    <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-info shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                            Events</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 6 ?></div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Number of Members Card Example -->
-                    <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-warning shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                            Members</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 4 ?></div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
                                     </div>
                                 </div>
                             </div>
--- notes.md
+++ notes.md
@@ -5,6 +5,10 @@
 `May 26, 2021`
 
 - Completed the UI setup for the admin pages, the home page, the tenants login & registration page.
-
+  ![Admin-login-page](repo_images%5CAdmin-login.png)
+  ![Admin-dashboard](repo_images%5CAdmin-Dashboard.png)
+  ![admin-register-page](repo_images%5CAdmin-Register.png)
+  ![Manage-adminUsers-page](repo_images%5CAdmin-Users-manage.png)
 - I added a feature that allows the super-admin to register other users and assign different roles to them. The roles currently available are super_admin and sub_admin. and also only the super admin can modify the role of a current admin_user, activate/deactivate their account, and also delete their account.
+
 - The login and logout date and time of every admin can be seen by the super admin.
